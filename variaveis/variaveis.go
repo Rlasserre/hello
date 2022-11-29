f7b25b2cb2027122b39357c240173726ed2b6efa
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-11-29 16:11:41 -0300

    Update

--- variaveis/variaveis.go
+++ variaveis/variaveis.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+// No go posso inferir as variaveis
 
 func main() {
-	var nome = "Rafael"
+	nome := "Rafael"
 	var versao float32 = 1.1
 	var idade = 28
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade, "anos.")
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println(reflect.TypeOf(nome), reflect.TypeOf(idade), reflect.TypeOf(versao))
 }
