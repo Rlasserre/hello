654ebe4bf9617e46a6a9e946afb8455992485bbf
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-11-29 16:08:28 -0300

    update

--- variaveis/variaveis.go
+++ variaveis/variaveis.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Rafael"
+	var nome = "Rafael"
 	var versao float32 = 1.1
-	var idade int = 28
+	var idade = 28
 	fmt.Println("Olá, sr.", nome, "sua idade é", idade, "anos.")
 	fmt.Println("Este programa está na versão", versao)
 }
